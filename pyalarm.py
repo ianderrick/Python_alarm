98f7930f50ca9e7a4b95779c4c9e5ad7327cda88
Ian Derrick <15677606+ianderrick@users.noreply.github.com>
2022-12-20 08:08:27 -0800

    Update pyalarm.py

--- pyalarm.py
+++ pyalarm.py
@@ -27,37 +27,37 @@ stop_alarm_button.pack()
 
 # Function to set the alarm
 def set_alarm():
-    # Get the current time
+# Get the current time
     current_time = datetime.datetime.now()
 
-    # Get the desired alarm time
+# Get the desired alarm time
     desired_time = datetime.datetime.strptime(time_input.get(), "%H:%M")
 
-    # Calculate the time until the alarm should go off
+# Calculate the time until the alarm should go off
     time_difference = desired_time - current_time
     time_until_alarm = time_difference.seconds
 
-    # Enable the stop alarm button
+# Enable the stop alarm button
     stop_alarm_button.config(state="normal")
 
-    # Schedule the alarm to go off
+# Schedule the alarm to go off
     root.after(time_until_alarm * 1000, sound_alarm)
 
 # Function to sound the alarm
 def sound_alarm():
-    # Play the alarm sound
+# Play the alarm sound
     alarm_sound.play()
 
-    # Disable the set alarm button and enable the stop alarm button
+# Disable the set alarm button and enable the stop alarm button
     set_alarm_button.config(state="disabled")
     stop_alarm_button.config(state="normal")
 
 # Function to stop the alarm
 def stop_alarm():
-    # Stop the alarm sound
+# Stop the alarm sound
     alarm_sound.stop()
 
-    # Enable the set alarm button and disable the stop alarm button
+# Enable the set alarm button and disable the stop alarm button
     set_alarm_button.config(state="normal")
     stop_alarm_button.config(state="disabled")
 
